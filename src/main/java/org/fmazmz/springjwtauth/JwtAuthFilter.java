4edb6f4092dd59db5fcfec5c4d1db881537a9d1b
WHITEROSE <firasmoussa60@gmail.com>
2026-05-23 19:32:08 +0200

    create initial auth header filter

---
+++ src/main/java/org/fmazmz/springjwtauth/JwtAuthFilter.java
@@ -0,0 +1,24 @@
+package org.fmazmz.springjwtauth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        // missing token
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            doFilterInternal(request, response, filterChain);
+        }
+    }
+}
---
+++ src/main/java/org/fmazmz/springjwtauth/Role.java
@@ -0,0 +1,6 @@
+package org.fmazmz.springjwtauth;
+
+public enum Role {
+    USER,
+    ADMIN
+}
